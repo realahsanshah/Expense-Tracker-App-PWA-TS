7932b61c11b82353bbedbc6e98620b7ad376c2ed
ahsanshah <shahahsan56@gmail.com>
2020-09-04 18:57:27 +0500

    Added History Component

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,3 +1,19 @@
 import React,{useContext} from 'react'
 import { GlobalContext } from './../context/GlobalState';
+import TransactionItem from './TransactionItem';
 
+const History=()=>{
+    const {transactions}=useContext(GlobalContext);
+
+    return(
+        <div>
+            <h4>History</h4>
+            <ul className='list'>
+                {transactions.map(trans=><TransactionItem key={trans.id} trans={trans} />)}
+            </ul>
+        </div>
+    )
+
+}
+
+export default History;
