55432356c9fa2d142eefff9dc50632ee831e686c
ahsanshah <shahahsan56@gmail.com>
2020-09-04 18:31:46 +0500

    Rewrote GlobalState

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,39 +1,50 @@
-import React,{createContext,useReducer} from 'react';
+import React,{createContext,useReducer} from 'react'
 import AppReducer from './AppReducer';
 
+type stateType={
+    transactions:number[],
+    deleteTransaction:any,
+    addTransaction:any
+}
 
-const initialState={
-    transactions:[]
+const initialState:stateType={
+    transactions:[],
+    deleteTransaction:null,
+    addTransaction:null,
 }
 
 
 export const GlobalContext=createContext(initialState);
 
+export const GlobalProvider=({children}:{children:any})=>{
+    const [state,dispatch]=useReducer(AppReducer,initialState)
 
-export const GlobalProvider=({children})=>{
-    const [state,dispatch]=useReducer(AppReducer,initialState);
-
-    const deleteTransaction=(id)=>{
+    const deleteTransaction=(id:any)=>{
         dispatch({
             type:'REMOVE_TRANSACTION',
             payload:id
         })
     }
 
-    const addTransaction=(text,amount)=>{
+    const addTransaction=(text:string,amount:string)=>{
         dispatch({
             type:'ADD_TRANSACTION',
             payload:{
-                text:text,
-                amount:amount
+                text,
+                amount
             }
         })
     }
 
-    return (<GlobalContext.Provider value={{
-        transactions:state.transactions,
-        deleteTransaction,addTransaction
-    }}>
-        {children}
-    </GlobalContext.Provider>)
+    return (
+        <GlobalContext.Provider 
+            value={{
+                transactions:state.transactions,
+                deleteTransaction,
+                addTransaction
+            }}>
+                {children}
+        </GlobalContext.Provider>
+    )
+
 }
