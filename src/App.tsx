0bea22b73bae48765cf6941fa25a5189996bfb29
ahsanshah <shahahsan56@gmail.com>
2020-09-04 19:16:03 +0500

    Got App from project 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import History from './components/History';
+import Transaction from './components/Transaction';
+import {GlobalProvider} from './context/GlobalState';
+
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <GlobalProvider>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpense />
+        <History />
+        <Transaction />
+      </div>
+      </GlobalProvider> 
     </div>
   );
 }
